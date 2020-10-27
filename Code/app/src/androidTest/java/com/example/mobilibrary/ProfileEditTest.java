bf030003cb9a3a2d91affcb74d3a4c07cd50dcb6
Jill <jzzheng@ualberta.ca>
2020-10-27 16:01:40 -0600

    "Modified files related to ProfileActivity"

--- Code/app/src/androidTest/java/com/example/mobilibrary/ProfileEditTest.java
+++ Code/app/src/androidTest/java/com/example/mobilibrary/ProfileEditTest.java
@@ -1,4 +1,66 @@
 package com.example.mobilibrary;
 
+import android.app.Activity;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Test class for ProfileActivity. All the UI tests are written here.
+ *  Robotium test framework is used.
+ */
+
+@RunWith(AndroidJUnit4.class)
 public class ProfileEditTest {
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<ProfileActivity> rule =
+            new ActivityTestRule<>(ProfileActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    /**
+     * Gets the Activity
+     *
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+    /**
+     * Check
+     */
+    @Test
+    public void check() {
+        solo.assertCurrentActivity("Wrong activity!", ProfileActivity.class);
+    }
+
+
+    /**
+     * Close activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+
 }
--- Code/app/src/main/java/com/example/mobilibrary/ProfileActivity.java
+++ Code/app/src/main/java/com/example/mobilibrary/ProfileActivity.java
@@ -1,13 +1,166 @@
 package com.example.mobilibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.common.primitives.Chars;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private ImageButton editButton;
+    private TextView usernameText;
+    private TextView emailText;
+    private TextView phoneText;
+    private EditText editEmail;
+    private EditText editPhone;
+    private Button confirmButton;
+    private Button cancelButton;
+    private User profileUser;
+    private Context context;
+    // Change to DatabaseHelper once merged with master
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    // private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Returning to the previous activity where the user came from
+        final FloatingActionButton backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        // Get user information from database
+        // TODO: Get database from Nguyen's branch
+        // profileUser from previous activity AND then by searching database?
+        // Intent intent = getIntent();
+        // profileUsername = intent.get("profile");
+        // databaseHelper = new DatabaseHelper(this);
+        // DatabaseReference ref = database.getReference();
+        // profileUser = ref.child(userID);
+        // Temp Firebase placeholders
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        // placeholder User object, need to actually grab from database differently probably using databaseHelper
+        User currentUser = new User(user.getUid(), user.getDisplayName(), user.getEmail(), user.getPhoneNumber()); // get from Main/My Books Activity OR database
+
+
+        // Set variables
+        editButton = findViewById(R.id.edit_button);
+        usernameText = findViewById(R.id.username_text_view);
+        emailText = findViewById(R.id.email_text_view);
+        phoneText = findViewById(R.id.phone_text_view);
+        editEmail = findViewById(R.id.edit_email);
+        editPhone = findViewById(R.id.edit_phone);
+        confirmButton = findViewById(R.id.confirm_button);
+        cancelButton = findViewById(R.id.cancel_button);
+        context = getApplicationContext();
+
+        // Set TextViews
+        usernameText.setText(profileUser.getUsername());
+        emailText.setText(profileUser.getEmail());
+        phoneText.setText(profileUser.getPhone());
+
+        // Set visibility
+        if (!profileUser.getUsername().equals(currentUser.getUsername())) {
+            editButton.setVisibility(View.INVISIBLE);
+        }
+
+        emailText.setVisibility(View.INVISIBLE);
+        phoneText.setVisibility(View.INVISIBLE);
+        final List<View> toggleViews = new ArrayList<View>();
+        toggleViews.add(editEmail);
+        toggleViews.add(editPhone);
+        toggleViews.add(cancelButton);
+        toggleViews.add(confirmButton);
+        toggleViews.add(emailText);
+        toggleViews.add(phoneText);
+        toggleVisibility(toggleViews);
+
+        // User hitting edit button if they are viewing their own profile
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleViews.add(editButton);
+                toggleVisibility(toggleViews);
+                editEmail.setText(profileUser.getEmail());
+                editPhone.setText(profileUser.getPhone());
+
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        toggleVisibility(toggleViews);
+                    }
+                });
+
+                confirmButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!validateEmail(editEmail.getText().toString())) {
+                             Toast toast = Toast.makeText(context, "Entered an invalid email!", Toast.LENGTH_SHORT);
+                             toast.show();
+                        } else if (!TextUtils.isEmpty(editPhone.getText().toString())){ // Phone number input & length already restricted by layout
+                            // Update user with new email and/or phone in database
+                            // databaseHelper.updateUser(editEmail.getText().toString().trim(), editPhone.getText().toString().trim());
+                            toggleVisibility(toggleViews);
+                            Toast toast = Toast.makeText(context, "Profile saved!", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    }
+                });
+
+            }
+        });
+
     }
+
+    public void toggleVisibility(List<View> views) {
+        for (int i = 0; i < views.size(); i++) {
+            if (views.get(i).getVisibility() == View.VISIBLE) {
+                views.get(i).setVisibility(View.INVISIBLE);
+            } else {
+                views.get(i).setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    /**
+     * Checks if the provided editText email is valid
+     * https://stackoverflow.com/questions/12947620/email-address-validation-in-android-on-edittext
+     * @param target
+     * @return true for valid email pattern, false otherwise
+     */
+    public boolean validateEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+
 }
