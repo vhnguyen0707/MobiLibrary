b6f2b15963700ed4ff03f4e816f55a317ddfe727
nataliahassan <hassan.natalia@yahoo.ca>
2020-10-31 13:17:46 -0600

    changes to BookDetails

--- Code/app/src/main/java/com/example/mobilibrary/BookDetailsFragment.java
+++ Code/app/src/main/java/com/example/mobilibrary/BookDetailsFragment.java
@@ -1,6 +1,9 @@
 package com.example.mobilibrary;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -55,6 +58,25 @@ public class BookDetailsFragment extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Book editedBook = viewBook;
+                editedBook.setTitle(title.getText().toString());
+                editedBook.setAuthor(author.getText().toString());
+
+                String stringISBN = ISBN.getText().toString().replaceAll(" ", "");
+                int isbn = Integer.parseInt(stringISBN);
+                editedBook.setISBN(isbn);
+
+                if (!nullPhoto()) {
+                    BitmapDrawable drawable = (BitmapDrawable) photo.getDrawable();
+                    Bitmap bitmap = drawable.getBitmap();
+                    editedBook.setImage(bitmap);
+                } else {
+                    editedBook.setImage(null);
+                }
+                
+                Intent editedIntent = new Intent();
+                editedIntent.putExtra("edited book", editedBook);
+                setResult(2, editedIntent);
                 finish();
             }
         });
@@ -81,6 +103,17 @@ public class BookDetailsFragment extends AppCompatActivity {
         });
     }
 
+    private boolean nullPhoto () {
+        Drawable drawable = photo.getDrawable();
+        BitmapDrawable bitmapDrawable;
+        if (!(drawable instanceof BitmapDrawable)) {
+            bitmapDrawable = null;
+        } else {
+            bitmapDrawable = (BitmapDrawable) photo.getDrawable();
+        }
+        return drawable == null || bitmapDrawable.getBitmap() == null;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -89,10 +122,11 @@ public class BookDetailsFragment extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // pass edited book back to parent activity
                 Book editedBook = (Book) data.getSerializableExtra("edited");
-                Intent editedIntent = new Intent();
-                editedIntent.putExtra("edited book", editedBook);
-                setResult(2, editedIntent);
-                finish();
+                title.setText(editedBook.getTitle());
+                author.setText(editedBook.getAuthor());
+                // owner.setText(editedBook.getOwner().getUsername());
+                ISBN.setText(editedBook.getISBN());
+                photo.setImageBitmap(editedBook.getImage());
             }
         }
     }
