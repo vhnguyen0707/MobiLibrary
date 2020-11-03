769b4cb0f7f36950fb8a2ffa2f10a5390c04d561
Jill <jzzheng@ualberta.ca>
2020-11-03 14:10:57 -0700

    Added manual test (for demo purposes) for viewing another user's profile

--- Code/app/src/main/java/com/example/mobilibrary/HomeFragment.java
+++ Code/app/src/main/java/com/example/mobilibrary/HomeFragment.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mobilibrary.Activity.ProfileActivity;
 import com.example.mobilibrary.DatabaseController.DatabaseHelper;
@@ -19,7 +21,6 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
  */
 public class HomeFragment extends Fragment {
 
-    private FloatingActionButton profileButton;
     private DatabaseHelper databaseHelper = new DatabaseHelper(getContext());
 
     public HomeFragment() {
@@ -30,7 +31,8 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        profileButton = (FloatingActionButton) view.findViewById(R.id.profile);
+
+        FloatingActionButton profileButton = (FloatingActionButton) view.findViewById(R.id.profile);
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +42,19 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        TextView otherProfile = (TextView) view.findViewById(R.id.other_profile);
+        otherProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ProfileActivity.class);
+                intent.putExtra("profile",
+                        "test123");
+                startActivity(intent);
+            }
+        });
+
+
         // Inflate the layout for this fragment
         return view;
     }
