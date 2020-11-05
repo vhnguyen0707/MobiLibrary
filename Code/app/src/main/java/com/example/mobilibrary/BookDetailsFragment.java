175233a9f11440420b19bb290abf31b7c7b6a4fd
nataliahassan <hassan.natalia@yahoo.ca>
2020-11-04 20:22:13 -0700

    Comments and Documentation added

--- Code/app/src/main/java/com/example/mobilibrary/BookDetailsFragment.java
+++ Code/app/src/main/java/com/example/mobilibrary/BookDetailsFragment.java
@@ -27,35 +27,46 @@ import java.io.ByteArrayOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * This class takes in a book and displays its details (Title, Author, Owner, ISBN and Status),
+ * requests currently on the book, and, if available, the book's photograph.
+ * Additionally, this class can toggle between displaying the book details and the list of requests on the book
+ */
 public class BookDetailsFragment extends AppCompatActivity {
-    TextView title;
-    TextView author;
-    TextView owner;
-    TextView ISBN;
-    TextView authorTitle;
-    TextView ownerTitle;
-    TextView isbnTitle;
-    FloatingActionButton backButton;
-    FloatingActionButton editButton;
-    FloatingActionButton deleteButton;
-    String req_users [] = {"Natalia", "Chloe", "Kimberly", "Jill", "Nguyen", "Sooraj"}; // sort of a placeholder this one, need to replace with actual requesting users once we implement it
-    Button detailsBtn;
-    Button requestsBtn;
-    TextView[] requestAssets;
-    ImageView photo;
-    ListView reqList;
-    ArrayAdapter<String> reqAdapter;
-    ArrayList<String> reqDataList;
+    private TextView title;
+    private TextView author;
+    private TextView owner;
+    private TextView ISBN;
+    private TextView status;
+    private TextView ownerTitle;
+    private TextView isbnTitle;
+    private TextView statusTitle;
+    private FloatingActionButton backButton;
+    private FloatingActionButton editButton;
+    private FloatingActionButton deleteButton;
+    private String req_users [] = {"Natalia", "Chloe", "Kimberly", "Jill", "Nguyen", "Sooraj"}; // sort of a placeholder this one, need to replace with actual requesting users once we implement it
+    private Button detailsBtn;
+    private Button requestsBtn;
+    private TextView[] requestAssets;
+    private ImageView photo;
+    private ListView reqList;
+    private ArrayAdapter<String> reqAdapter;
+    private ArrayList<String> reqDataList;
 
+    /**
+     * Creates the activity for viewing books and the requests on them, and the necessary logic to do so
+     * @param SavedInstances The book to be viewed
+     */
     @Override
     protected void onCreate (@Nullable Bundle SavedInstances) {
         super.onCreate(SavedInstances);
         setContentView(R.layout.layout_book_details_fragment);
 
-        // photo option is separate user story, will come back to it
+        // set each variable to correct view
         title =  findViewById(R.id.view_title);
         author = findViewById(R.id.view_author);
         owner = findViewById(R.id.view_owner);
+        status = findViewById(R.id.view_status);
         ISBN = findViewById(R.id.view_isbn);
         backButton = findViewById(R.id.back_to_books_button);
         editButton = findViewById(R.id.edit_button);
@@ -64,33 +75,32 @@ public class BookDetailsFragment extends AppCompatActivity {
         detailsBtn = findViewById(R.id.detailsBtn);
         requestsBtn = findViewById(R.id.reqBtn);
         reqList = findViewById(R.id.reqListView);
-        authorTitle = findViewById(R.id.view_author_title);
         ownerTitle = findViewById(R.id.view_owner_title);
         isbnTitle = findViewById(R.id.view_isbn_title);
-        requestAssets = new TextView[]{title, author, authorTitle, owner, ownerTitle,ISBN, isbnTitle, };
-        reqDataList =new ArrayList<>();
+        statusTitle = findViewById(R.id.view_status_title);
+
+        // hide request list at open of activity
+        requestAssets = new TextView[]{title, author, owner, status, ownerTitle,ISBN, isbnTitle, statusTitle };
+        reqDataList = new ArrayList<>();
         for (String user: req_users){
             reqDataList.add(user + "has requested your book");
         }
         reqAdapter =  new ArrayAdapter<String>(this,R.layout.req_custom_list, R.id.textView, reqDataList);
         reqList.setAdapter(reqAdapter);
         reqList.setVisibility(View.GONE);
-        // get book from intent
+
+        // check that a book was passed to this activity, otherwise end the activity
         if (getIntent() == null) {
             finish();
         }
-
         final Book viewBook = (Book) getIntent().getSerializableExtra("view book");
 
-        System.out.println("Book name: " +  viewBook.getTitle());
-
+        // fill fields with correct information from the passed book
         title.setText(viewBook.getTitle());
         author.setText(viewBook.getAuthor());
-
-
-
-        // owner.setText(viewBook.getOwner().getUsername());
+        owner.setText(viewBook.getOwner().getUsername());
         ISBN.setText(viewBook.getISBN());
+        status.setText(viewBook.getStatus());
         Bitmap bitmap;
         if (viewBook.getImage() != null) {
             bitmap = BitmapFactory.decodeByteArray(viewBook.getImage(), 0,
@@ -99,8 +109,11 @@ public class BookDetailsFragment extends AppCompatActivity {
             bitmap = null;
         }
         photo.setImageBitmap(bitmap);
-        photo.setImageBitmap(bitmap);
 
+        /**
+         * If Back Button is pressed, return to list of owned books, any changes in the book will be saved
+         * and the book's information updated accordingly
+         */
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -110,10 +123,9 @@ public class BookDetailsFragment extends AppCompatActivity {
                         (ISBN.equals(viewBook.getISBN()))){
                     viewBook.setTitle(title.getText().toString());
                     viewBook.setAuthor(author.getText().toString());
+                    viewBook.setISBN(ISBN.getText().toString().replaceAll(" ", ""));
 
-                    String stringISBN = ISBN.getText().toString().replaceAll(" ", "");
-                    viewBook.setISBN(stringISBN);
-
+                    // if a book has a photo pass along the photo's bitmap
                     if (!nullPhoto()) {
                         Bitmap bitmap = ((BitmapDrawable)photo.getDrawable()).getBitmap();
                         ByteArrayOutputStream outStream = new ByteArrayOutputStream();
@@ -121,9 +133,10 @@ public class BookDetailsFragment extends AppCompatActivity {
                         byte[] bookImage = outStream.toByteArray();
                         viewBook.setImage(bookImage);
                     } else {
-                        viewBook.setImage(null);
+                        viewBook.setImage(null);    // book has no photo so image bitmap is set to null
                     }
 
+                    // return the book with its changed fields
                     Intent editedIntent = new Intent();
                     editedIntent.putExtra("edited book", viewBook);
                     setResult(2, editedIntent);
@@ -132,41 +145,35 @@ public class BookDetailsFragment extends AppCompatActivity {
             }
         });
 
+        /**
+         * If Delete Button is pressed, return to list of owned books and pass this book along as marked
+         * as to be deleted
+         */
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Book deleteBook = viewBook;
                 Intent deleteIntent = new Intent();
-                deleteIntent.putExtra("delete book", deleteBook);
+                deleteIntent.putExtra("delete book", viewBook); // mark book to be deleted
                 setResult(1, deleteIntent);
                 finish();
             }
         });
 
+        /**
+         * If Edit Button is pressed, open EditBookFragment activity and pass it the book to edit its fields
+         */
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Book editBook = viewBook;
-
-                editBook.setTitle(title.getText().toString());
-                editBook.setAuthor(author.getText().toString());
-                editBook.setISBN(ISBN.getText().toString());
-                if (!nullPhoto()) {
-                    Bitmap bitmap = ((BitmapDrawable)photo.getDrawable()).getBitmap();
-                    ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
-                    byte[] bookImage = outStream.toByteArray();
-                    viewBook.setImage(bookImage);
-                } else {
-                    viewBook.setImage(null);
-                }
-
                 Intent editIntent = new Intent(BookDetailsFragment.this, EditBookFragment.class);
-                editIntent.putExtra("edit", editBook);
+                editIntent.putExtra("edit", viewBook);
                 startActivityForResult(editIntent, 2);
             }
         });
 
+        /**
+         * Toggles view, shows request list and hides book details
+         */
         requestsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -180,6 +187,10 @@ public class BookDetailsFragment extends AppCompatActivity {
 
             }
         });
+
+        /**
+         * Toggles view, hides request list and shows book details
+         */
         detailsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -191,17 +202,28 @@ public class BookDetailsFragment extends AppCompatActivity {
         });
     }
 
+    /**
+     * Determines if the book's photograph has a null bitmap
+     * @return boolean true if the book's photograph has a null bitmap, false otherwise
+     */
     private boolean nullPhoto () {
-        Drawable drawable = photo.getDrawable();
+        Drawable drawable = photo.getDrawable();    // get image
         BitmapDrawable bitmapDrawable;
         if (!(drawable instanceof BitmapDrawable)) {
-            bitmapDrawable = null;
+            bitmapDrawable = null;  // image has no bitmap
         } else {
-            bitmapDrawable = (BitmapDrawable) photo.getDrawable();
+            bitmapDrawable = (BitmapDrawable) photo.getDrawable();  // get image bitmap
         }
-        return drawable == null || bitmapDrawable.getBitmap() == null;
+        return drawable == null || bitmapDrawable.getBitmap() == null;  // determine if bitmap is null
     }
 
+    /**
+     * Logic for returning from EditBookFragment activity, if requestCode is 2 and resultCode is RESULT_OK
+     * then edit the corresponding fields to match the passed book
+     * @param requestCode 2 if book is returned from the edit activity
+     * @param resultCode RESULT_OK if book is returned from the edit activity
+     * @param data Book object passed from the edit activity
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
