00d7ad0e403c3c054fc5f47a288b897989045b51
Chloe Ching <cching@ualberta.ca>
2020-11-08 21:43:04 -0700

    Added click listener to recycle view

--- Code/app/src/main/java/com/example/mobilibrary/BookListAdaptor.java
+++ Code/app/src/main/java/com/example/mobilibrary/BookListAdaptor.java
@@ -1,12 +1,24 @@
 package com.example.mobilibrary;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mobilibrary.DatabaseController.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.Blob;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
@@ -17,31 +29,19 @@ public class BookListAdaptor extends RecyclerView.Adapter<BookListAdaptor.MyView
     private List<String> mAuthors;
     private List<String> mISBNS;
     private List<String> mStatuses;
+    private List<String> mOwners;
+    private List<Blob> mImages;
+    private Context mContext;
 
-
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-
-        public TextView title;
-        public TextView author;
-        public TextView isbn;
-        public TextView status;
-
-        public MyViewHolder(View v) {
-            super(v);
-            title = (TextView) v.findViewById(R.id.book_title);
-            author = (TextView) v.findViewById(R.id.book_author);
-            isbn = (TextView) v.findViewById(R.id.book_isbn);
-            status = (TextView) v.findViewById(R.id.book_status);
-        }
-
-    }
-
-
-    public BookListAdaptor(List<String> titles, List<String> authors, List<String> isbns, List<String> statuses) {
+    public BookListAdaptor(Context context, List<String> titles, List<String> authors, List<String> isbns, List<String> statuses, List<String> owners, List<Blob> images) {
         mTitles = titles;
         mAuthors = authors;
         mISBNS = isbns;
         mStatuses = statuses;
+        mOwners = owners;
+        mImages = images;
+        mContext = context;
+
 
     }
 
@@ -59,7 +59,7 @@ public class BookListAdaptor extends RecyclerView.Adapter<BookListAdaptor.MyView
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         holder.title.setText(mTitles.get(position));
@@ -67,11 +67,77 @@ public class BookListAdaptor extends RecyclerView.Adapter<BookListAdaptor.MyView
         holder.isbn.setText(mISBNS.get(position));
         holder.status.setText(mStatuses.get(position));
 
+        // ***include check later to only show available/requested books
+
+        //click listener
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //get image of book clicked
+                byte[] bookImage = null;
+                if (mImages.get(position) != null) {
+                    bookImage = mImages.get(position).toBytes();
+                }
+
+                //Get the User object from currently clicked book by going into firestore
+                final FirebaseFirestore db = FirebaseFirestore.getInstance();
+                DocumentReference docRef = db.collection("Users").document(mOwners.get(position));
+                final byte[] finalBookImage = bookImage;
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+                        System.out.println("Document snapshot data: " + document.getData());
+                        System.out.println("Document snapshot data email: " + document.get("email"));
+
+                        String username = document.get("username").toString();
+                        String email = document.get("email").toString();
+                        String name = document.get("name").toString();
+                        String phoneNo = document.get("phoneNo").toString();
+
+                        User user = new User(username, email, name, phoneNo);
+
+                        //method to initiate book details intent
+                        initIntent(user);
+
+                    }
+                    public void initIntent(User user){
+                        //get the book details of currently clicked item
+                        Book newBook = new Book(mTitles.get(position), mISBNS.get(position), mAuthors.get(position), mStatuses.get(position), finalBookImage, user);
+                        Intent viewBook = new Intent(mContext, BookDetailsFragment.class);
+                        viewBook.putExtra("view book", newBook);
+                        mContext .startActivity(viewBook);
+                    }
+                });
+            }
+        });
     }
+
+
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mTitles.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView title;
+        public TextView author;
+        public TextView isbn;
+        public TextView status;
+        CardView parentLayout;
+
+        public MyViewHolder(View v) {
+            super(v);
+            title = (TextView) v.findViewById(R.id.book_title);
+            author = (TextView) v.findViewById(R.id.book_author);
+            isbn = (TextView) v.findViewById(R.id.book_isbn);
+            status = (TextView) v.findViewById(R.id.book_status);
+            parentLayout = (CardView) v.findViewById(R.id.parent_layout);
+        }
+
     }
 }
--- Code/app/src/main/java/com/example/mobilibrary/HomeFragment.java
+++ Code/app/src/main/java/com/example/mobilibrary/HomeFragment.java
@@ -3,7 +3,9 @@ package com.example.mobilibrary;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -15,6 +17,15 @@ import com.example.mobilibrary.Activity.ProfileActivity;
 import com.example.mobilibrary.DatabaseController.DatabaseHelper;
 import com.example.mobilibrary.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Blob;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Homepage fragment that can be navigated to using navigation bar. Contains search bar, and access to the User profile.
@@ -25,6 +36,16 @@ public class HomeFragment extends Fragment {
 
     private DatabaseHelper databaseHelper = new DatabaseHelper(getContext());
 
+    private RecyclerView booksRV;
+    private RecyclerView.Adapter mAdaptor;
+
+    private List<String> titles = new ArrayList<>();
+    private List<String> authors = new ArrayList<>();
+    private List<String> isbns = new ArrayList<>();
+    private List<String> statuses = new ArrayList<>();
+    private List<String> owners = new ArrayList<>();
+    private List<Blob> images = new ArrayList<>();
+
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -34,7 +55,36 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Go to the profile activity for the current logged in user
+        //each time homepage is opened will show all books from collection
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("Books").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                titles.clear();
+                authors.clear();
+                isbns.clear();
+                statuses.clear();
+                owners.clear();
+                images.clear();
+
+                for (DocumentSnapshot snapshot : value) {
+                    titles.add(snapshot.getString("Title"));
+                    authors.add(snapshot.getString("Author"));
+                    isbns.add(snapshot.getString("ISBN"));
+                    statuses.add(snapshot.getString("Status"));
+                    //owners.add(snapshot.getString("Owner"));
+                    owners.add(String.valueOf(snapshot.get("Owner")));
+                    images.add(snapshot.getBlob("Image"));
+                }
+                mAdaptor = new BookListAdaptor(getContext(), titles, authors, isbns, statuses, owners, images);
+                booksRV.setAdapter(mAdaptor);
+            }
+        });
+        booksRV = (RecyclerView) view.findViewById(R.id.booksRV);
+
+
+        //When profile is clicked
         FloatingActionButton profileButton = (FloatingActionButton) view.findViewById(R.id.profile);
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +96,7 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        // TODO: Delete after implementing list for all books in database (this is a test for demo)
+        // Test for clicking another profile
         TextView otherProfile = (TextView) view.findViewById(R.id.other_profile);
         otherProfile.setOnClickListener(new View.OnClickListener() {
             @Override
