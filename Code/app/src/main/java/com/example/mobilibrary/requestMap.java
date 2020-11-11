3dad17ef190c4203494dc5fc0e549d07979d21cc
Kimberly Tran <ktran5@ualberta.ca>
2020-11-11 15:52:09 -0700

    implemented autofill

--- Code/app/src/main/java/com/example/mobilibrary/requestMap.java
+++ Code/app/src/main/java/com/example/mobilibrary/requestMap.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,15 +19,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.firebase.database.annotations.NotNull;
 
-import java.io.IOException;
-import java.util.List;
+import java.util.Arrays;
 
 public class requestMap extends FragmentActivity implements OnMapReadyCallback{
     private LatLng newLatLng;
+    private String TAG = "requestMap";
     private GoogleMap map;
     private Button confirmButton;
-    private SearchView searchButton;
+    private AutocompleteSupportFragment searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,41 +42,37 @@ public class requestMap extends FragmentActivity implements OnMapReadyCallback{
                 .findFragmentById(R.id.map);
 
         if (!Places.isInitialized()) {
-            Places.initialize(getApplicationContext(), "YOUR_API_KEY");
+            Places.initialize(getApplicationContext(), "AIzaSyADFmERhLf1R3L2B1LDfe38bBcN4m1vtLo");
         }
 
-        searchButton = findViewById(R.id.search_bar);
         confirmButton = findViewById(R.id.confirm_button);
 
         mapFragment.getMapAsync(this);
 
-        searchButton.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                String location = searchButton.getQuery().toString();
-                List<Address> addressList = null;
-                if(location !=null || location != ""){
-                    Geocoder geocoder = new Geocoder(requestMap.this);
-                    try {
-                        addressList = geocoder.getFromLocationName(location,1);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    Address address = addressList.get(0);
-                    Float zoom = 16.0f;
-                    newLatLng = new LatLng(address.getLatitude(),address.getLongitude());
-                    map.addMarker(new MarkerOptions().position(newLatLng).title("Location"));
-                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(newLatLng,zoom));
+        // Initialize the AutocompleteSupportFragment.
+        searchButton = (AutocompleteSupportFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.autocomplete_fragment);
 
-                }
-                return false;
+        // Specify the types of place data to return.
+        searchButton.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        searchButton.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NotNull Place place) {
+                newLatLng = new LatLng(place.getLatLng().latitude,place.getLatLng().longitude);
+                float zoom = 16.0f;
+                map.addMarker(new MarkerOptions().position(newLatLng).title("Location") );
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(newLatLng,zoom));
             }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
+            public void onError(@NotNull Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
             }
         });
+
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
